66513d8662f020239211d694d03c7ad8c545973d
abraham ghaemi <ig.ghaemi@gmail.com>
2019-02-16 00:20:30 +0330

    update namespace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use App\Providers\KnowledgeBaseArticle;
+use App\KnowledgeBaseArticle;
 
 class AppServiceProvider extends ServiceProvider
 {
