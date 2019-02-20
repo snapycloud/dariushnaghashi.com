28de7e010731488933a547cfeb76c3ccc36f05b2
abraham ghaemi <ig.ghaemi@gmail.com>
2019-02-20 04:30:04 +0330

    update bio

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -6,12 +6,17 @@
             <h2 class="section-title mb-30 text-center">Who is Behind?</h2>
             <figure><img src="{{ asset('style/images/art/about4.jpg') }}" alt=""></figure>
             <div class="space30"></div>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit, fusce dapibus, tellus ac.</p>
-            <blockquote class="bordered">
-              <p>Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula lacinia odio sem nec elit.</p>
-              <footer class="blockquote-footer">Connor Gibson</footer>
-            </blockquote>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <p>Beyond The Lens "Building a Brand You Can Be Proud Of"
+The quickest and easiest way to increase your Brand perceived value is to create professional content (photo or video) that speaks to the character & power of your brand.</p>
+            
+              <p>I know that capturing imagery that best represent your brand is a very important moment in your career... so I take the proper steps to ensure I understand your goals so we can powerfully communicate that through the content we create.</p>
+              
+            <p>It comes with a lot of big decisions but the results are amazing... Instead of new random snap shots or video's with NO meaning, we CREATE meaningful content that instantly improve your ability to attract the right type of clients.</p>
+            <p>
+              My personal success as a talent, being featured on .... 
+            </p>
+
+            <h3>I look forward to the journey!!!</h3>
             <ul class="social social-mute social-s mb-15 text-center">
               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
--- resources/views/components/home_video_wrapper.blade.php
+++ resources/views/components/home_video_wrapper.blade.php
@@ -14,7 +14,7 @@
           <h2 class="section-title">Featured</h2>
           <div class="meta larger">Darush Naghashi</div>
           <div class="space10"></div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A eos et, suscipit, accusamus qui nemo amet modi vero nulla dolorem debitis deleniti tempora praesentium facilis eius asperiores, reiciendis fugit perferendis.</p>
+          <p>I BELIEVE GREAT SERVICE IS GREAT FOR BUSINESS. SO I BUILT A PRODUCTION COMPANY THAT DELIVERS OUTSTANDING CONTENT WITH AMAZING SERVICE</p>
           <div class="space10"></div>
           <a href="{{ route('videos') }}" class="btn btn-full-rounded btn-white">Check All Videos</a> </div>
         <!--/column --> 
