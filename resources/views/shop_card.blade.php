64a4966cdda11a6190718ce526f95165610c0044
abraham ghaemi <snapycloud@gmail.com>
2019-04-03 04:00:01 +0430

    update shop

--- resources/views/shop_card.blade.php
+++ resources/views/shop_card.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.master')
 
 @section('header')
-    <div class="wrapper image-wrapper bg-image inverse-text" data-image-src="style/images/art/bg2.jpg" style="background-image: url(&quot;style/images/art/bg2.jpg&quot;);">
+    <div class="wrapper image-wrapper bg-image inverse-text" data-image-src="{{ asset('style/images/art/slider-bg6.jpg') }}" style="background-image: url({{ asset('style/images/art/slider-bg6.jpg') }})">
     <div class="container inner pt-120 pb-120 text-center">
       <h1 class="heading mb-0">SHOP CARD TEST</h1>
     </div>
