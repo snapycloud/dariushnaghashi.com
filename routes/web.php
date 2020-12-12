372e2c73db3a449c03b6d394e63da80028e4d463
abraham ghaemi <ig.ghaemi@gmail.com>
2020-12-12 04:21:17 +0330

    update routes

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::get('image/{id}', function($id){
 	header('Pragma: public');
 	header('Cache-Control: max-age=360000, must-revalidate');
 	header('Content-Type: ' . $cat->type);
-	readfile('/home/production/onpermise/5c96a5002b4e543eb/data/upload/' . $cat->id );
+	readfile('/usr/share/nginx/crm/On-premises/crm.dariushnaghashi.com/data/upload/' . $cat->id );
 })->name('image');
